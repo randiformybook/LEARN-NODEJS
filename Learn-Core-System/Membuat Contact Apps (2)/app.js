b9be18d0e730741527dbdc69a7cb4cd1159c33fd
Randi Himawan <randi.mybook@gmail.com>
2023-09-13 22:14:48 +0700

    Mengupdate Learn-Core-System/Membuat Contact Apps (2)

--- Learn-Core-System/Membuat Contact Apps (2)/app.js
+++ Learn-Core-System/Membuat Contact Apps (2)/app.js
@@ -1,46 +1,45 @@
-const yargs = require('yargs');
+const yargs = require("yargs");
 //const { saveContacts } = require('./contact-system');
-const contact = require('./contact-system.js');
+const contact = require("./contact-system.js");
 
 // yargs.command('add','Menambahkan contact baru', () =>{},(argv)=>{
 //     console.log(argv.nama);
 // });
 
 //yargs.command
-yargs
-    .command({
-        command : 'add',
-        describe : 'Memasukan Contact Baru',
-        builder: {
-            nama:{
-                describe : 'Masukan Nama Lengkap',
-                demandOption  : true,
-                type:'string',
-                alias:"n"
-            },
-            noHP:{
-                describe : 'Silahkan masukan no HP anda',
-                demandOption : true,
-                type         : "string",
-                alias        : "hp"
-            },
-            email:{
-                describe :'Masukan email anda',
-                demandOption   :false,
-                default        :"Belum memasukan email",
-                type           : "email",
-                alias          : "e"
-            }
-        },
-        handler:(argv) =>{
-            // const contact = {
-            //     nama : argv.nama,
-            //     noHP : argv.noHP,
-            //     email : argv.email
-            // };
-            // console.log([contact])
-            contact.saveContacts(argv.nama,argv.noHP,argv.email);
-        },
-    });
+yargs.command({
+  command: "add",
+  describe: "Memasukan Contact Baru",
+  builder: {
+    nama: {
+      describe: "Masukan Nama Lengkap",
+      demandOption: true,
+      type: "string",
+      alias: "n",
+    },
+    noHP: {
+      describe: "Silahkan masukan no HP anda",
+      demandOption: true,
+      type: "string",
+      alias: "hp",
+    },
+    email: {
+      describe: "Masukan email anda",
+      demandOption: false,
+      default: "Belum memasukan email",
+      type: "email",
+      alias: "e",
+    },
+  },
+  handler: (argv) => {
+    // const contact = {
+    //     nama : argv.nama,
+    //     noHP : argv.noHP,
+    //     email : argv.email
+    // };
+    // console.log([contact])
+    contact.saveContacts(argv.nama, argv.noHP, argv.email);
+  },
+});
 
 yargs.parse();
--- Learn-Core-System/Membuat Contact Apps (2)/contact-system.js
+++ Learn-Core-System/Membuat Contact Apps (2)/contact-system.js
@@ -1,7 +1,7 @@
 // TODO : Connect ke FileSystem NodeJS
-const fs = require('node:fs');
-const chalk = require('chalk');
-const validator = require('validator');
+const fs = require("node:fs");
+const chalk = require("chalk");
+const validator = require("validator");
 //const readline = require('node:readline');
 
 // const rl =  readline.Interface({
@@ -11,15 +11,15 @@ const validator = require('validator');
 
 //membuat folder data kalau belum ada
 const dirPath = "../scripts/data";
-if(!fs.existsSync(dirPath)){
-    fs.mkdirSync(dirPath);
-};
+if (!fs.existsSync(dirPath)) {
+  fs.mkdirSync(dirPath);
+}
 
 //membuat file contact.json apabila belum ada
 const fileContacts = "../scripts/data/contacts.json";
-if (!fs.existsSync(fileContacts)){
-    fs.writeFileSync(fileContacts,"[]","utf-8");
-};
+if (!fs.existsSync(fileContacts)) {
+  fs.writeFileSync(fileContacts, "[]", "utf-8");
+}
 
 //function dari questioner
 // const questioner = (questions) => {
@@ -30,46 +30,63 @@ if (!fs.existsSync(fileContacts)){
 //     });
 // };
 
-const saveContacts = (nama,noHP,email) =>{
-    const dataInput = {nama,noHP,email};
-    const readData = fs.readFileSync('../scripts/data/contacts.json','utf-8');
-    //NOTE : JSON memiliki perilaku seperti array, jadi data dari dataInput dijadikan dalam bentu JSON, sehingga nanti kita bisa menggunakan metode push untuk memasukan data
-    const saveData = JSON.parse(readData);
+const saveContacts = (nama, noHP, email) => {
+  const dataInput = { nama, noHP, email };
+  const readData = fs.readFileSync("../scripts/data/contacts.json", "utf-8");
+  //NOTE : JSON memiliki perilaku seperti array, jadi data dari dataInput dijadikan dalam bentu JSON, sehingga nanti kita bisa menggunakan metode push untuk memasukan data
+  const saveData = JSON.parse(readData);
 
-    //check duplicate
-    const duplicate = saveData.find((contact) =>{
-        return [
-            contact.nama === nama,
-            contact.email === email,
-            contact.noHP === noHP
-        ]});
-    if(duplicate){
-        console.log(chalk.bgRed.bold("Data yang Anda masukan,sudah terdaftar!, Mohon di periksa kembali"));
-        return false;
-    };
-    if (email){
-        if(!validator.isEmail(email)){
-            console.log(chalk.bgRed.bold("Email yang Anda masukan tidak valid, Silahkan di periksa kembali"));
-            return false;
-        }
+  //check duplicate
+  const duplicate = saveData.find((contact) => {
+    return [
+      contact.nama === nama,
+      contact.email === email,
+      contact.noHP === noHP,
+    ];
+  });
+  if (duplicate) {
+    console.log(
+      chalk.bgRed.bold(
+        "Data yang Anda masukan,sudah terdaftar!, Mohon di periksa kembali"
+      )
+    );
+    return false;
+  }
+  if (email) {
+    if (!validator.isEmail(email)) {
+      console.log(
+        chalk.bgRed.bold(
+          "Email yang Anda masukan tidak valid, Silahkan di periksa kembali"
+        )
+      );
+      return false;
     }
-    if(!validator.isMobilePhone(noHP,'id-ID')){
-        console.log(chalk.bgRed.bold("No Handphone yang Anda masukan tidak valid, Silahkan di periksa kembali"));
-        return false;
-    }
+  }
+  if (!validator.isMobilePhone(noHP, "id-ID")) {
+    console.log(
+      chalk.bgRed.bold(
+        "No Handphone yang Anda masukan tidak valid, Silahkan di periksa kembali"
+      )
+    );
+    return false;
+  }
 
-    //Push saveData yang sudah dalam bentuk JSON ke dalam JSON  variabel saveData, sehingga ini akan menambah index savedata beru ke dalam array of JSON
-    saveData.push(dataInput);
+  //Push saveData yang sudah dalam bentuk JSON ke dalam JSON  variabel saveData, sehingga ini akan menambah index savedata beru ke dalam array of JSON
+  saveData.push(dataInput);
 
-    //Sekarang data yang sudah ada di variabel saveData itu akan di masukan atau ditulis ke file contacts.json
-    fs.writeFileSync('../scripts/data/contacts.json',JSON.stringify(saveData));
-    //console.log(convertToJSON);
-    console.log(chalk.bold.bgGreenBright('Terimah Kasih, data sudah masuk di data contact kami'));
+  //Sekarang data yang sudah ada di variabel saveData itu akan di masukan atau ditulis ke file contacts.json
+  fs.writeFileSync("../scripts/data/contacts.json", JSON.stringify(saveData));
+  //console.log(convertToJSON);
+  console.log(
+    chalk.bold.bgGreenBright(
+      "Terimah Kasih, data sudah masuk di data contact kami"
+    )
+  );
 
-    // rl.close();
-}
+  // rl.close();
+};
 
-    module.exports = {
-        //questioner,
-        saveContacts
-    };
+module.exports = {
+  //questioner,
+  saveContacts,
+};
