e141d66a6b050e498f6edeadf84a0c1c81d5ad68
Randi Himawan <randi.mybook@gmail.com>
2023-10-08 19:47:41 +0700

    Membuat Folder Baru : Express Contact App 3

---
+++ Express Contact App/Express Contact App 3/app.js
@@ -0,0 +1,173 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const app = express();
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const flash = require("connect-flash");
+const { body, validationResult, check } = require("express-validator");
+const {
+  loadContacts,
+  findContact,
+  addContact,
+  checkDuplicateNama,
+  checkDuplicateEmail,
+  checkDuplicatePhone,
+} = require("./utils/contacts-system.js");
+const port = 3000;
+
+//Gunakan EJS
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+//Built-in Middleware : Express static
+app.use(express.static("public"));
+//Built-in Middleware : Express urlencoded
+app.use(express.urlencoded({ extended: true }));
+//Flash Configuration
+app.use(cookieParser("secret"));
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+    cookie: { maxAge: 6000 },
+  })
+);
+app.use(flash());
+
+app.get("/", (req, res) => {
+  // res.sendFile('index.html',{root : __dirname});
+  const mahasiswa = [
+    {
+      nama: "Randi Himawan",
+      umur: 2023 - 1989,
+      email: "hrandi1989@gmail.com",
+    },
+    {
+      nama: "Putri Swandayani",
+      umur: 2023 - 1990,
+      email: "puu_tri@gmail.com",
+    },
+    {
+      nama: "Udo",
+      umur: 2023 - 1989,
+      email: "udo@gmail.com",
+    },
+  ];
+  res.render("index", {
+    mahasiswa,
+    title: "Home",
+    layout: "layouts/main-layout",
+  });
+});
+
+app.get("/about", (req, res) => {
+  // res.sendFile('about.html',{root : __dirname});
+  res.render("about", {
+    title: "Halaman About",
+    layout: "layouts/main-layout",
+  });
+});
+
+// Halaman contact
+app.get("/contact", (req, res) => {
+  const contacts = loadContacts();
+
+  res.render("contact", {
+    title: "Halaman Contact",
+    layout: "layouts/main-layout",
+    contacts,
+    msg: req.flash("msg"),
+  });
+});
+
+//Halaman Penambahan Contact
+app.get("/contact/add", (req, res) => {
+  res.render("add-contact", {
+    title: "Halaman Penambahan Contact",
+    layout: "layouts/main-layout",
+  });
+});
+
+// proses penambahaan contact
+app.post(
+  "/contact",
+  [
+    // Validator of Nama (Custome Validator)
+    body("nama").custom((value) => {
+      const duplicate = checkDuplicateNama(value);
+      if (duplicate) {
+        throw new Error(`${value} sudah terdaftar di sistem Kontak!`);
+      }
+      return true;
+    }),
+    // Validator of Email
+    body("email")
+      .custom((value) => {
+        const duplicate = checkDuplicateEmail(value);
+        if (duplicate) {
+          throw new Error(`email sudah terdaftar di sistem Kontak!`);
+        }
+        return true;
+      })
+      .isEmail()
+      .optional({ checkFalsy: true })
+      .normalizeEmail()
+      .withMessage("email yang anda masukan tidak valid"),
+
+    // Validator of Handphone number
+    body("nohp")
+      .custom((value) => {
+        const duplicate = checkDuplicatePhone(value);
+        if (duplicate) {
+          throw new Error(
+            `No HandPhone yang dimasukan sudah terdaftar di sistem Kontak!`
+          );
+        }
+        return true;
+      })
+      .isMobilePhone()
+      .withMessage("No Handphone yang anda masukan tidak valid"),
+    //   check("nohp", "no Handphone yang anda masukan tidak valid").isMobilePhone(
+    //     "id-ID"
+    // ),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      //   return res.status(400).json({ errors: errors.array() });
+      res.render("add-contact", {
+        title: "Halaman Penambahan Contact",
+        layout: "layouts/main-layout",
+        errors: errors.array(),
+      });
+    } else {
+      addContact(req.body);
+      // kirimkan flash message
+      req.flash("msg", "data Contact berhasil ditambahkan ke Daftar Kontak!");
+      res.redirect("/contact");
+    }
+    // console.log(req.body);
+    //res.send(req.body);
+  }
+);
+
+//Halaman detail Contact
+app.get("/contact/:nama", (req, res) => {
+  const contact = findContact(req.params.nama);
+
+  res.render("detail", {
+    title: "Halaman Detail Contact",
+    layout: "layouts/main-layout",
+    contact,
+  });
+});
+//app.use biasanya digunakan untuk menanganin file/ data yang tidak ada, jadi apapun yg tidak ada, akan dikembalikan ke app.use
+//jangan letakan ini diatas, karena nt apapun datanya, walaupun ada, akan tetep menjalankan app.use dulu, sehingga app.get belum sempat jalan tapi hasil sudah keluar dari app.use
+app.use("/", (req, res) => {
+  res.status(404);
+  res.send("<h1>Error : 404</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
---
+++ Express Contact App/Express Contact App 3/utils/contacts-system.js
@@ -0,0 +1,75 @@
+const fs = require("node:fs");
+
+//membuat folder data kalau belum ada
+const dirPath = "./data";
+if (!fs.existsSync(dirPath)) {
+  fs.mkdirSync(dirPath);
+}
+
+//membuat file contact.json apabila belum ada
+const fileContacts = "./data/contacts.json";
+if (!fs.existsSync(fileContacts)) {
+  fs.writeFileSync(fileContacts, "[]", "utf-8");
+}
+//ambil semua contact yang ada di contact.json
+const loadContacts = () => {
+  const readData = fs.readFileSync("./data/contacts.json", "utf-8");
+  //NOTE : JSON memiliki perilaku seperti array, jadi data dari dataInput dijadikan dalam bentu JSON, sehingga nanti kita bisa menggunakan metode push untuk memasukan data
+  const saveData = JSON.parse(readData);
+  return saveData; //return array of contacts
+};
+
+// cari contact berdasarkan nama
+const findContact = (nama) => {
+  const contacts = loadContacts();
+  const contact = contacts.find(
+    (contact) => contact.nama.toLowerCase() === nama.toLowerCase()
+  );
+  return contact;
+};
+
+// menuliskan/menimpa file contacts.json dengan data yang baru
+const saveContacts = (contacts) => {
+  fs.writeFileSync("./data/contacts.json", JSON.stringify(contacts));
+};
+
+// menambahkan contact yang baru
+const addContact = (contact) => {
+  const contacts = loadContacts();
+  contacts.push(contact);
+  saveContacts(contacts);
+};
+
+// Check duplicate
+// const checkDuplicate = (nama) => {
+//   const contacts = loadContacts();
+//   return contacts.find((contact) => contact.nama === nama);
+// };
+
+const checkDuplicateNama = (nama) => {
+  const contacts = loadContacts();
+  return contacts.find((contact) => {
+    return contact.nama === nama;
+  });
+};
+
+const checkDuplicateEmail = (email) => {
+  const contacts = loadContacts();
+  return contacts.find((contact) => {
+    return contact.email === email;
+  });
+};
+const checkDuplicatePhone = (nohp) => {
+  const contacts = loadContacts();
+  return contacts.find((contact) => {
+    return contact.nohp === nohp;
+  });
+};
+module.exports = {
+  loadContacts,
+  findContact,
+  addContact,
+  checkDuplicateNama,
+  checkDuplicateEmail,
+  checkDuplicatePhone,
+};
